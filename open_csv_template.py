b633f730f5c0d775cabc9604e46447bb8bf962b1
Erik Lacson <elacson@gmail.com>
2016-02-02 10:23:51 +0800

    converted approval number fields in psc to 4 numeric character format

--- open_csv_template.py
+++ open_csv_template.py
@@ -1,10 +1,17 @@
 import pandas as pd
 import numpy as np
 
-all_psc_data = pd.read_csv("./data_files/all_psc.csv")
+all_psc_data = pd.read_csv("data_files/all_psc.csv")
+cofin_grants_loan_data = pd.read_csv("data_files/cofin_grants_loans.csv")
 
-print all_psc_data.loc[['approval number'], ['title']]
+all_psc_data = all_psc_data.astype(str)
 
+psc_convert = all_psc_data.copy()
 
+psc_convert['approval number'] = all_psc_data['approval number'].apply(lambda x: x.zfill(4))
 
+psc_convert['approval number'] = all_psc_data['approval number'].apply(lambda x: x.zfill(4))
 
+psc_convert.to_csv("data_files/psc_convert.csv")
+
+print psc_convert['approval number']
