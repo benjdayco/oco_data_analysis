ac9aff38380a3900cf46e4814352606b20b5be67
Erik Lacson <elacson@gmail.com>
2016-02-16 13:26:05 +0800

    added block for separating approval numbers

--- cofin_separate_approval_numbers.py
+++ cofin_separate_approval_numbers.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import numpy as np
 
-# Set some pandas options for controlling output display
-pd.set_option('display.max_columns', 10)
-pd.set_option('display.max_rows', 10)
 
 #read data file
 cofin_grants_loan_data_raw = pd.read_csv("data_files/loan_grants_test.csv")
+
+#temporary dataframe 
+temp_df = pd.DataFrame()
 
 #remove records with all blank columns (noise from lotus notes)
 cofin_grants_loan_data = cofin_grants_loan_data_raw.dropna(subset = ['Approval Numbers','Proj No'] , how='all')
@@ -17,25 +17,29 @@ cofin_grants_loan_data_short = cofin_grants_loan_data[['Partner', 'Approval Numb
 	
 #iterate each row
 for index,row in cofin_grants_loan_data_short.iterrows():
-
-	print row['Proj No']
-
 	#temporary store approval number of current row
 	approval_number_orig = str(row['Approval Numbers'])
 
 	#split approval number into array by character
 	approval_number_split =  approval_number_orig.split("/")
+
 	
 	#itererate through each approval number in array
 	for number in approval_number_split:
-		print number
- 
-	#insert current approval number into new dataframe with row[Project No]
-	new_row = row
-	new_row
-	print new_row
-	#insert row as series
-	# add number to series
+		
+		new_row = row #store new_row in temporary Series
+		new_row['indvidual approval number'] = number #add number as new column
+		
+		#insert current approval number into new dataframe with row[Project No]
+		temp_df = temp_df.append(new_row, ignore_index=True)
+		new_df = temp_df
+
+		#print new_row
+
+		# add new_row to new datagrame
+print new_df
+
+
 
 
 
