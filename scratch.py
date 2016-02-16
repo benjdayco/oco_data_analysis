c0f00c7149d2866639fd0f97c7506a90167e799b
Erik Lacson <elacson@gmail.com>
2016-02-16 10:58:49 +0800

    test code segment for adding Series to Dataframe: scratch.py

--- scratch.py
+++ scratch.py
@@ -6,12 +6,14 @@ temp_df = pd.DataFrame()
 count = 0
 
 for index, row in df.iterrows():
-	count = count + 1
 	new_row = row
-	new_row['new col'] = count
-	
-	temp_df = temp_df.append(new_row, ignore_index=True)
-	new_df = temp_df
+	string = row['String']
+	string_split =  string.split("/")
+
+	for new_string in string_split:
+		new_row['String'] = new_string
+		temp_df = temp_df.append(new_row, ignore_index=True)
+		new_df = temp_df
 
 print new_df
 	
